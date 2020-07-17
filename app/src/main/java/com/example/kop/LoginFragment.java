73c2885b2c8503454f3067dbb08813538b97ddf9
pserdax <parahatgeldiyew.s@gmail.com>
2020-07-17 15:14:00 +0300

    implemented bottom Navigation, NavGraph & Login page

---
+++ app/src/main/java/com/example/kop/LoginFragment.java
@@ -0,0 +1,99 @@
+package com.example.kop;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.kop.databinding.FragmentLoginBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.concurrent.Executor;
+
+public class LoginFragment extends Fragment {
+    private FirebaseAuth mAuth;
+    private FragmentLoginBinding fragmentLoginBinding;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        //Check if user is signed in (not-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        final NavController navController = Navigation.findNavController(view);
+        mAuth = FirebaseAuth.getInstance();
+        EditText email = fragmentLoginBinding.loginEmail;
+        EditText password = fragmentLoginBinding.loginPassword;
+        TextView txtRegister = fragmentLoginBinding.tvRegister;
+        Button btnLogin = fragmentLoginBinding.btnLogin;
+
+        txtRegister.setOnClickListener( v -> {
+            navController.navigate(R.id.action_loginFragment_to_registerFragment);
+        });
+
+        btnLogin.setOnClickListener( v ->{
+
+            String mEmail = email.getText().toString().trim();
+            String mPassword = password.getText().toString().trim();
+
+            if(TextUtils.isEmpty(mEmail)){
+                email.setError("Email is required");
+                return;
+            }
+            if(TextUtils.isEmpty(mPassword)){
+                password.setError("Password is required");
+                return;
+            }
+
+            fragmentLoginBinding.progressBar.setVisibility(View.VISIBLE);
+
+            mAuth.signInWithEmailAndPassword(mEmail,mPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()) {
+                        Toast.makeText(getActivity(), "Logged in Successfully!", Toast.LENGTH_SHORT).show();
+                        navController.navigate(R.id.action_loginFragment_to_mainActivity);
+                    } else {
+                        Toast.makeText(getActivity(), "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        fragmentLoginBinding.progressBar.setVisibility(View.GONE);
+                    }
+                }
+                    });
+
+
+
+
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/kop/MenuFragment.java
@@ -0,0 +1,69 @@
+package com.example.kop;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.kop.databinding.FragmentMenuBinding;
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
+
+public class MenuFragment extends Fragment {
+    private FragmentMenuBinding fragmentMenuBinding;
+    private Fragment fragment = null;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_menu, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        fragmentMenuBinding.chipNavigationBar.setItemSelected(R.id.home, true);
+        getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.container, new HomeFragment()).commit();
+
+        fragmentMenuBinding.chipNavigationBar.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(int i) {
+                switch (i) {
+                    case R.id.hot:
+                        fragment = new HotFragment();
+                        Log.i("TAG", "hot");
+                        break;
+
+                    case R.id.notification:
+                        fragment = new NotificationFragment();
+                        Log.i("TAG", "notification");
+                        break;
+
+                    case R.id.bookmark:
+                        fragment = new FavoriteFragment();
+                        Log.i("TAG", "profile");
+                        break;
+
+                    case R.id.alien:
+                        fragment = new HomeFragment();
+                        break;
+
+
+                }
+                if (fragment != null) {
+                    getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+                }
+            }
+        });
+
+
+    }
+}
